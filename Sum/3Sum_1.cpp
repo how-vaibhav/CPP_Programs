ba2cfdbede41ab5b5fa92bff448376ace7e8398b
mai-VAIBHAV <vaibhavtiwari10505@gmail.com>
2026-01-16 17:56:39 +0530

    Refactor 3Sum implementation for clarity and efficiency

--- Sum/3Sum_1.cpp
+++ Sum/3Sum_1.cpp
@@ -1,8 +1,30 @@
 #include<iostream>
 #include<vector>
 #include<set>
+#include<algorithm>
 using namespace std;
 int main(){
-vector<vector<int>> 
-    
+vector<int> nums = {-1, 0, 1, 2, -1, -4};
+int n=nums.size();
+set <vector<int>> s;
+
+for(int i=0 ; i<n ; i++){
+    for(int j=i+1 ; j<n; j++){
+        for(int k =j+1 ;k<n ;k++){
+            int sum = nums[i] + nums[j] + nums[k];
+                if(sum==0){
+                    vector<int> triplet ={nums[i] , nums[j] , nums[k]};
+                    sort(triplet.begin() , triplet.end());
+                    s.insert(triplet);
+                }
+        }
+    }
+}
+
+//
+for(auto x : s){
+    cout<<"[";
+    for(int i : x) cout<<i<<"  ";
+    cout<<"] "<<endl;
+}
 }
